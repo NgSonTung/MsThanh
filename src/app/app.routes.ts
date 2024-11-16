41a33d05764f78384ec1552026eb51772dc7240c
ngsontung <ngsondecon@gmail.com>
2024-11-16 17:34:50 +0700

    hehehehe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { InterpolationComponent } from './interpolation/interpolation.component';
-import {ClassStyleBindingComponent} from "./class-style-binding/class-style-binding.component";
+import { ClassStyleBindingComponent } from './class-style-binding/class-style-binding.component';
+import { PropBindingComponent } from './prop-binding/prop-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 export const routes: Routes = [
   {
@@ -8,7 +10,15 @@ export const routes: Routes = [
     component: InterpolationComponent,
   },
   {
-    path:'class-style-binding',
-    component: ClassStyleBindingComponent
-  }
+    path: 'class-style-binding',
+    component: ClassStyleBindingComponent,
+  },
+  {
+    path: 'prop-binding',
+    component: PropBindingComponent,
+  },
+  {
+    path: 'event-binding',
+    component: EventBindingComponent,
+  },
 ];
